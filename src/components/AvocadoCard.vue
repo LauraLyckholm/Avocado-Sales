0db5558e32f69b125e4407e0de4f67a428ce8334
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-11-13 19:57:53 +0100

    Started on the avocado cards and got data to show

---
+++ src/components/AvocadoCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <article class="card" v-for="dataItem in data" :key="dataItem.id">
+        <h2>{{ dataItem.region }} on {{ dataItem.date }}</h2>
+        <p>Average price: ${{ dataItem.averagePrice }}</p>
+        <p>Total volume: {{ dataItem.totalVolume.toFixed(0) }}</p>
+        <p>Total bags sold: {{ dataItem.totalBagsSold.toFixed(0) }}</p>
+        <p>Small bags sold: {{ dataItem.smallBagsSold.toFixed(0) }}</p>
+        <p>Large bags sold: {{ dataItem.largeBagsSold.toFixed(0) }}</p>
+    </article>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+
+const { data } = defineProps(["data"])
+
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid var(--color-text);
+    padding: 2rem;
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,63 @@
+<template>
+  <div v-if="isLoading">
+    <h2>Loading avocado sales...</h2>
+    <LottieAnimation :animation-data="AvocadoAnimation" :auto-play="true" :loop="true" :speed="1" ref="animation" />
+  </div>
+  <main v-else>
+    <h1>Top 20 avocado sales in Northen American regions</h1>
+    <section class="avocado-card">
+      <AvocadoCard :data="avocadoData" />
+    </section>
+  </main>
+</template>
+
 <script setup>
 import { onMounted, ref } from "vue"
 import { LottieAnimation } from "lottie-web-vue"
 import AvocadoAnimation from "../assets/animations/avocado-small.json"
+import AvocadoCard from "../components/AvocadoCard.vue"
 
 const animation = ref()
 const avocadoData = ref([])
+const isLoading = ref(true)
 
-const API = "https://avocado-sales-api.onrender.com/avocado-sales"
+const API = "https://avocado-sales-api.onrender.com/avocado-sales/top-20"
 
 onMounted(async () => {
-  // If you want to do something with the animation after a delay, you can do it here
-  setTimeout(() => {
-    // Do something with animation.value here
-  }, 500)
+  // // If you want to do something with the animation after a delay, you can do it here
+  // setTimeout(() => {
+  //   // Do something with animation.value here
+  // }, 500)
 
   try {
     const response = await fetch(API)
     const data = await response.json()
     avocadoData.value = data
-    console.log(avocadoData.value);
+    isLoading.value = false
   } catch (error) {
     console.error(error)
+    isLoading.value = false
   }
 })
 
 </script>
 
-<template>
-  <LottieAnimation :animation-data="AvocadoAnimation" :auto-play="true" :loop="true" :speed="1" ref="animation" />
-  <p>{{ avocadoData.length }}</p>
-</template>
+<style lang="scss" scoped>
+.avocado-card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 
-<style scoped></style>
+  @media (min-width: 767px) and (max-width: 1023px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+  }
+
+  @media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+  }
+}
+</style>
